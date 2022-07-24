1fe8108f3b11896f6804e64920f52c3fa5ed308d
zhuzhouhao <2021110464@stu.sicnu.edu.cn>
2022-07-24 08:10:53 +0800

    Changes

--- python网络爬虫与信心提取/Scrapy爬虫框架.py
+++ python网络爬虫与信心提取/Scrapy爬虫框架.py
@@ -8,4 +8,22 @@
     2.  SCHEDULER (REQUESTS) -> ENGINE (REQUESTS) -> DOWNLOADER (RESPONSE) -> ENGINE (RESPONSE) -> SPIDERS
     3.  SPIDERS (ITEMS/REQUESTS) -> ENGINE (ITEMS)    -> ITEM PIPELINES
                                            (REQUESTS) -> SCHEDULER
+在ENGINE和DOWNLOADER中间有一个中间件Downloader Middleware
+    目的：实施Engine,Scheduler和Downloader之间进行用户可配置的控制
+    功能：修改，丢弃，新增请求或响应
+
+Spider
+    解析Downloader返回的响应(Response)
+    产生爬取项(scraped item)
+    产生额外的爬取请求(Request)
+
+Item Pipelines
+    以流水线方式处理Spider产生的爬取项
+    有一组操作顺序组成，类似流水线，每个操作是一个Item Pipeline类型
+    可能操作包括:清洗，检验和查重爬取项中的HTML数据，将数据存储到数据库
+
+在SPIDERS和ENGINE中间有一个中间件Spider Middleware
+    目的：对请求和爬取项的再处理
+    功能：修改，丢弃，新增请求和爬取项
+
 '''
