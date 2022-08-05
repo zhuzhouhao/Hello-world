bafaae7c67c233810d743f6aec96fc5fccedfdab
zhuzhouhao <2021110464@stu.sicnu.edu.cn>
2022-08-05 15:28:35 +0800

    java_doc

--- Java_study/Java_study/src/demo.java
+++ Java_study/Java_study/src/demo.java
@@ -1,32 +1,7 @@
-import java.util.Scanner;
 public class demo
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        double height = scanner.nextDouble();
-        double weight = scanner.nextDouble();
-
-        //write your code here......
-        enum STATUS {ÃçÌõ,Æ«Êİ,ÊÊÖĞ,Æ«ÅÖ};
-        STATUS state;
-        double IBM = weight / (Math.pow(height,2));
-        if(IBM < 18.5)
-        {
-            state = STATUS.ÃçÌõ;
-        }
-        else if (IBM < 20.9)
-        {
-            state = STATUS.Æ«Êİ;
-        }
-        else if (IBM < 24.9)
-        {
-            state = STATUS.ÊÊÖĞ;
-        }
-        else
-        {
-            state = STATUS.Æ«ÅÖ;
-        }
-        System.out.println(state);
+        System.out.println("hello ÖĞ¹ú");
     }
 }
